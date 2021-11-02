196c5b53830efab71fad949386c5f08799d80f04
KaizOffical <anhkhangphan4@gmail.com>
2021-11-02 10:14:18 +0700

    update command handler

--- index.js
+++ index.js
@@ -3,7 +3,10 @@ const client = new Client()
 
 const { prefix, token } = require('./config.json')
 
-client.commands = new Collection()
+client.commands = new Collection();
+client.aliases = new Collection();
+
+require('./handler/command')(client);
 
 client.on('ready', () => {
   console.log(`${client.user.tag} is ready`)
@@ -13,17 +16,16 @@ client.on('ready', () => {
   .catch(console.error);
 })
 
-//when user send a message in guild have a bot with async function
 client.on('message', async message => {
-  if(message.author.bot) return; // if author of command is a bot, your bot will return
-  if(!message.guild) return; // if message or command in DM, return
-  if (!message.content.startsWith(prefix)) return; // if message do not start with prefix, return
+  if(message.author.bot) return;
+  if(!message.guild) return;
+  if (!message.content.startsWith(prefix)) return;
 
-  switch(message.content.toLowerCase()) {
-    case `${prefix}ping`:
-      message.channel.send(`Pong! **\`${client.ws.ping}ms\`**`)
-      break;
-  }
+  const args = message.content.slice(prefix.length).trim().split(/ +g/);
+  const cmd = args.shift().toLowerCase();
+  if(cmd.length === 0) return;
+  let command = client.commands.get(cmd) || client.commands.get(client.aliases.get(cmd));
+  if(command) command.run(client, message, args);
 });
 
 client.login(token);
